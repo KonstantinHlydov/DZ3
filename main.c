3c96468d272b26fb06a24d6f6a1da9f784c4b22a
KonstantinHlydov <61344850+KonstantinHlydov@users.noreply.github.com>
2020-03-10 00:47:31 +0300

    Хлудов Константин - Группа 1142 - Домашние задание

--- main.c
+++ main.c
@@ -1,43 +1,51 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <conio.h>
 #define for_i for(int i = 0; i < n; i++)
 
-void sort(int n, int *v)
+void swap(int* a, int* b)
 {
-	int q;
-	for (int i = 1; i < (n - 1); i++)
+	int z = *a;
+	*a = *b;
+	*b = z;
+	
+}
+
+void sort(int *n, int *v)
+{
+	for (int i = 0; i < n; i++)
 	{
-		for (int j = (n - 1); j > 0; j--)
+		for (int j = 1; j < n; j++)
 		{
 
 			if (v[j] < v[j - 1])
 			{
-				q = v[j];
-				v[j] = v[j - 1];
-				v[j - 1] = q;
+					swap(&v[j],&v[j-1]);
 			}
 		}
 
 	}
 }
 
+
+
 int main()
 {
 	int n;
 	printf("Vvedite razmer massiva:");
 	scanf("%d", &n);
-	int* v = (int*)calloc(n, sizeof(int));
+	int *v = (int*)calloc(n, sizeof(int));
 	printf("Sozdal i zapolnil massiv:");
 	for_i
 	{
-		v[i] = rand() % 20;
-	    printf("%d ", v[i]);
+	v[i] = n-i;
+	printf("%d ", v[i]);
 	}
 	printf("\n");
 	sort(n, v);
 	printf("Rezyltat sortirovki:");
 	for_i
 	{
-		printf("%d ", v[i]);
+	printf("%d ", v[i]);
 	}
 }
